f9de02d7c33454e84ca1624e31419cad8ce204d1
يحيى حمود <fantasy-art@live.com>
2023-07-24 01:11:16 +0300

    Model expense done

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -2,15 +2,24 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
-class Expense {
-  final String id;
-  final String title;
-  final double amount;
-  final DateTime dateTime;
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+}
 
+class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.dateTime,
+    required this.category,
   }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime dateTime;
+  final Category category;
 }
