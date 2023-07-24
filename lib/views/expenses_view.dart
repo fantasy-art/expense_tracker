9be736cff0a5a8675db62072f8d09c48f684af0a
يحيى حمود <fantasy-art@live.com>
2023-07-24 09:37:43 +0300

    setting an AppBar with a title & Action

--- lib/views/expenses_view.dart
+++ lib/views/expenses_view.dart
@@ -14,6 +14,12 @@ class _ExpensesViewState extends State<ExpensesView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),
